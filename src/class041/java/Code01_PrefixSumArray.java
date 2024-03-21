b684d7e7ae4e580c736c34755cc835aea5823c3e
happyfenglife <happyfenglife@qq.com>
2024-03-21 14:00:29 +0800

    区域累加和

---
+++ src/class041/java/Code01_PrefixSumArray.java
@@ -0,0 +1,18 @@
+// 利用前缀和快速得到区域累加和
+// 测试链接 : https://leetcode.cn/problems/range-sum-query-immutable/
+public class Solution {
+    class NumArray {
+        public int[] sum;
+
+        public NumArray(int[] nums) {
+            sum = new int[nums.length + 1];
+            for (int i = 1; i <= nums.length; i++) {
+                sum[i] = sum[i - 1] + nums[i - 1];
+            }
+        }
+
+        public int sumRange(int left, int right) {
+            return sum[right + 1] - sum[left];
+        }
+    }
+}
