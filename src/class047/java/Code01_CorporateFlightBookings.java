1c1b63cae161d7a94f4c76f556a8319a133f5382
happyfenglife <happyfenglife@qq.com>
2024-03-22 12:01:02 +0800

    航班预订统计

---
+++ src/class047/java/Code01_CorporateFlightBookings.java
@@ -0,0 +1,27 @@
+// 航班预订统计
+// 这里有 n 个航班，它们分别从 1 到 n 进行编号。
+// 有一份航班预订表 bookings ，
+// 表中第 i 条预订记录 bookings[i] = [firsti, lasti, seatsi]
+// 意味着在从 firsti 到 lasti
+//（包含 firsti 和 lasti ）的 每个航班 上预订了 seatsi 个座位。
+// 请你返回一个长度为 n 的数组 answer，里面的元素是每个航班预定的座位总数。
+// 测试链接 : https://leetcode.cn/problems/corporate-flight-bookings/
+public class Solution {
+    public static int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] cnt = new int[n + 2];
+        // 设置差分数组
+        for (int[] booking : bookings) {
+            cnt[booking[0]] += booking[2];
+            cnt[booking[1] + 1] -= booking[2];
+        }
+
+        for (int i = 1; i < cnt.length; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        int[] ans = new int[n];
+        System.arraycopy(cnt, 1, ans, 0, n);
+
+        return ans;
+    }
+}
