8a33f6d1a35f4f497ee62279c12c4f217dd3a8d2
happyfenglife <happyfenglife@qq.com>
2024-03-24 23:19:11 +0800

    判断一个数字是否是连续正整数的和

---
+++ src/class042/java/Code03_IsSumOfConsecutiveNumbers.java
@@ -0,0 +1,32 @@
+// 判断一个数字是否是若干数量(数量>1)的连续正整数的和
+public class Solution {
+    public static void main(String[] args) {
+        for (int num = 1; num < 200; num++) {
+            System.out.println(num + " : " + (is1(num) ? "T" : "F"));
+        }
+    }
+
+    public static boolean is1(int num) {
+        for (int start = 1, sum; start <= num; start++) {
+            sum = start;
+
+            for (int j = start + 1; j <= num; j++) {
+                if (sum + j > num) {
+                    break;
+                }
+
+                if (sum + j == num) {
+                    return true;
+                }
+
+                sum += j;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean is2(int num) {
+        return (num & (num - 1)) != 0;
+    }
+}
